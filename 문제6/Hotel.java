d19b02e25c63a07ab217a29409c1669925ad3539
jsang07 <68322203+jsang07@users.noreply.github.com>
2024-05-16 13:36:58 +0900

    Add files via upload

--- 문제6/Hotel.java
+++ 문제6/Hotel.java
@@ -1,10 +1,60 @@
-package test.test9;
+package test;
 
 public class Hotel {
-    void add(){};
-    void show(){};
+    private Room[] rooms;
 
-    static class Room{
+    public Hotel() {
+        rooms = new Room[11];
+        for (int i = 0; i < rooms.length; i++) {
+            rooms[i] = new Room(i);
+        }
+    }
 
+    public void add(int roomNumber, String guestName) {
+        if (roomNumber >= 0 && roomNumber < rooms.length) {
+            if (rooms[roomNumber].isReserved()) {
+                System.out.println(roomNumber + "번 방은 이미 예약되어 있습니다.");
+            } else {
+                rooms[roomNumber].reserve(guestName);
+            }
+        } else {
+            System.out.println("잘못된 객실 번호입니다.");
+        }
+    }
+
+    public void show() {
+        for (Room room : rooms) {
+            if (room.isReserved()) {
+                System.out.println(room.getRoomNumber() + "번 방을 " +room.getGuestName()+ "가 예약했습니다");
+
+            }
+        }
+    }
+
+    private class Room {
+        private int roomNumber;
+        private String guestName;
+        private boolean reserved = false;
+
+        public Room(int roomNumber) {
+            this.roomNumber = roomNumber;
+        }
+
+        public void reserve(String name) {
+            this.guestName = name;
+            this.reserved = true;
+        }
+
+        public boolean isReserved() {
+            return reserved;
+        }
+
+        public int getRoomNumber() {
+            return roomNumber;
+        }
+
+        public String getGuestName() {
+            return guestName;
+        }
     }
 }
---
+++ 문제6/Test9.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test9 {
+
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+        hotel.add(5, "홍길동");
+        hotel.add(2, "김철수");
+        hotel.show(); // 예약 현황 출력
+    }
+
+
+
+}
